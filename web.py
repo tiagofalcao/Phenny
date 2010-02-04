fc43fbb7188cf38889032474e64caf7279ae4b39
Tiago Rezende Campos Falcao <tiago@profusion.mobi>
2010-02-04 17:05:41 -0200

    Reformat web.py

--- web.py
+++ web.py
@@ -7,38 +7,38 @@ About: http://inamidst.com/phenny/
 
 import urllib
 
-class Grab(urllib.URLopener): 
-   def __init__(self, *args): 
-      self.version = 'Mozilla/5.0 (Phenny)'
-      urllib.URLopener.__init__(self, *args)
-   def http_error_default(self, url, fp, errcode, errmsg, headers): 
-      return urllib.addinfourl(fp, [headers, errcode], "http:" + url)
+class Grab(urllib.URLopener):
+    def __init__(self, *args):
+        self.version = 'Mozilla/5.0 (Phenny)'
+        urllib.URLopener.__init__(self, *args)
+    def http_error_default(self, url, fp, errcode, errmsg, headers):
+        return urllib.addinfourl(fp, [headers, errcode], "http:" + url)
 urllib._urlopener = Grab()
 
-def get(uri): 
-   if not uri.startswith('http'): 
-      return
-   u = urllib.urlopen(uri)
-   bytes = u.read()
-   u.close()
-   return bytes
+def get(uri):
+    if not uri.startswith('http'):
+        return
+    u = urllib.urlopen(uri)
+    bytes = u.read()
+    u.close()
+    return bytes
 
-def head(uri): 
-   if not uri.startswith('http'): 
-      return
-   u = urllib.urlopen(uri)
-   info = u.info()
-   u.close()
-   return info
+def head(uri):
+    if not uri.startswith('http'):
+        return
+    u = urllib.urlopen(uri)
+    info = u.info()
+    u.close()
+    return info
 
-def post(uri, query): 
-   if not uri.startswith('http'): 
-      return
-   data = urllib.urlencode(query)
-   u = urllib.urlopen(uri, data)
-   bytes = u.read()
-   u.close()
-   return bytes
+def post(uri, query):
+    if not uri.startswith('http'):
+        return
+    data = urllib.urlencode(query)
+    u = urllib.urlopen(uri, data)
+    bytes = u.read()
+    u.close()
+    return bytes
 
-if __name__=="__main__": 
-   main()
+if __name__=="__main__":
+    main()
